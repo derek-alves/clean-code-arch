62f1ef5b68ada989493c643c12afe8796b3ab99f
derek-alves <derek.alves1998@hotmail.com>
2022-08-11 01:51:04 -0300

    step: complete refactor

--- refactor/exercises/bin/example-two/example_two_after.dart
+++ refactor/exercises/bin/example-two/example_two_after.dart
@@ -1,59 +1,29 @@
 class ExampleTwoAfter {
-  static validate(str) {
-    if (str != null) {
-      if (str.isNotEmpty) {
-        if (str.length >= 11 || str.length <= 14) {
-          str = str
-              .replaceAll('.', '')
-              .replaceAll('.', '')
-              .replaceAll('-', '')
-              .replaceAll(" ", '');
+  static bool validate(String rawCpf) {
+    if (rawCpf.isEmpty) return false;
+    String cpf = _clean(rawCpf);
+    if (!_isValidLength(cpf)) return false;
+    if (_isOneCaracter(cpf)) return false;
 
-          if (!str.split("").every((c) => c == str[0])) {
-            try {
-              var d1, d2;
-              var dg1, dg2, rest;
-              var digito;
+    num digit1 = _calculateDigit(cpf, 10);
+    num digit2 = _calculateDigit(cpf, 11);
 
-              String nDigResult, nDigVerific = "";
+    String actualDigit = cpf.substring(9);
+    String calculatedDigit = '$digit1$digit2';
+    return actualDigit == calculatedDigit;
+  }
 
-              d1 = d2 = 0;
-              dg1 = dg2 = rest = 0;
+  static String _clean(String cpf) => cpf.replaceAll(RegExp(r"[^\s\w]"), '');
+  static bool _isValidLength(String cpf) => cpf.length == 11;
+  static bool _isOneCaracter(String cpf) =>
+      cpf.split("").every((digit) => digit == cpf[0]);
 
-              for (var nCount = 1; nCount < str.length - 1; nCount++) {
-                digito = int.parse(str.substring(nCount - 1, nCount));
-                d1 = d1 + (11 - nCount) * digito;
-
-                d2 = d2 + (12 - nCount) * digito;
-              }
-              rest = (d1 % 11);
-
-              dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-              dg2 += 2 * dg1;
-              rest = (d2 % 11);
-              if (rest < 2) {
-                dg2 = 0;
-              } else {
-                dg2 = 11 - rest;
-
-                nDigVerific =
-                    str.toString().substring(str.length - 2, str.length);
-              }
-              nDigResult = "" + dg1.toString() + "" + dg2.toString();
-              return nDigVerific == nDigResult;
-            } catch (error) {
-              print("Erro !" + error.toString());
-              return false;
-            }
-          } else {
-            return false;
-          }
-        } else {
-          return false;
-        }
-      }
-    } else {
-      return false;
+  static num _calculateDigit(String cpf, int factor) {
+    int total = 0;
+    for (var digit in cpf.split('')) {
+      if (factor > 1) total += int.parse(digit) * factor--;
     }
+    num rest = total % 11;
+    return (rest < 2) ? 0 : 11 - rest;
   }
 }
