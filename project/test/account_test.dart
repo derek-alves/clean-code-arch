decedb8cba0ef99ff70ea2b87c4435f60eb8c9d5
derek-alves <derek.alves1998@hotmail.com>
2022-08-13 15:05:30 -0300

    step: pass currency api fake to the all accounts instances

--- project/test/account_test.dart
+++ project/test/account_test.dart
@@ -1,28 +1,35 @@
 import 'package:project/src/account.dart';
 import 'package:test/test.dart';
 
-void main() {
-  late Account account;
+import 'currency_api_fake.dart';
 
-  setUpAll(() {
-    account = Account();
-  });
+void main() {
   test('Deve criar uma conta', () {
+    Account account = Account(CurrencyApiFake());
     final double balance = account.getBalance;
 
     expect(balance, equals(0));
   });
 
   test("Deve fazer um crédito de R\$100,00", () {
+    Account account = Account(CurrencyApiFake());
     account.credit(100);
     final balance = account.getBalance;
     expect(balance, equals(100));
   });
 
   test("Deve fazer um debit de R\$50,00", () {
+    Account account = Account(CurrencyApiFake());
     account.credit(100);
     account.debit(50);
     final balance = account.getBalance;
     expect(balance, equals(50));
+  });
+
+  test("Deve fazer um crédito de U\$100,00 com fake", () {
+    Account account = Account(CurrencyApiFake());
+    account.credit(100, "USD");
+    final balance = account.getBalance;
+    expect(balance, equals(500));
   });
 }
