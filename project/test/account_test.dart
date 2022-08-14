42aa9072e203d2d1d176a8ed8b780c4a2ffd5d37
derek-alves <derek.alves1998@hotmail.com>
2022-08-14 20:56:13 -0300

    step: create test mock, stub and spy

--- project/test/account_test.dart
+++ project/test/account_test.dart
@@ -1,9 +1,14 @@
+import 'package:mocktail/mocktail.dart';
 import 'package:project/src/account.dart';
+import 'package:project/src/currency_api.dart';
 import 'package:test/test.dart';
 
 import 'currency_api_fake.dart';
 
 void main() {
+  late CurrencyApiMock mockCurrencyApi;
+
+  setUp(() => mockCurrencyApi = CurrencyApiMock());
   test('Deve criar uma conta', () {
     Account account = Account(CurrencyApiFake());
     final double balance = account.getBalance;
@@ -32,4 +37,29 @@ void main() {
     final balance = account.getBalance;
     expect(balance, equals(500));
   });
+
+  test("Deve fazer um crédito de U\$100,00 com stub", () {
+    Account account = Account(mockCurrencyApi);
+
+    when(() => mockCurrencyApi.convert(any(), any())).thenAnswer(
+      (_) => 500,
+    );
+
+    var balance = account.credit(100, "USD");
+
+    expect(balance, equals(500));
+  });
+
+  test("Deve fazer um crédito de U\$100,00 com spy", () {
+    Account account = Account(mockCurrencyApi);
+
+    when(() => mockCurrencyApi.convert(any(), any())).thenAnswer(
+      (_) => 500,
+    );
+
+    account.credit(100, "USD");
+    verify(() => mockCurrencyApi.convert(any(), any())).called(1);
+  });
 }
+
+class CurrencyApiMock extends Mock implements CurrencyApi {}
