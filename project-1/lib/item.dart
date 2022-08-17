2470cec91eedd6f573c3502330aa38ba70f0fd22
derek-alves <derek.alves1998@hotmail.com>
2022-08-17 01:11:05 -0300

    step: create item(Product)

---
+++ project-1/lib/item.dart
@@ -0,0 +1,13 @@
+class Item {
+  final int id;
+  final String category;
+  final String description;
+  final double price;
+
+  Item({
+    required this.id,
+    required this.category,
+    required this.description,
+    required this.price,
+  });
+}
