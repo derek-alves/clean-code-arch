e1d236de518b6b6e04a03e58bee28dd41cbec93f
derek-alves <derek.alves1998@hotmail.com>
2022-08-18 18:47:35 -0300

    step: create tests for coupon

---
+++ project-1/test/coupon_test.dart
@@ -0,0 +1,24 @@
+import 'package:project/coupon.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("Deve criar um cupom de desconto válido", () {
+    final coupon = Coupon("VALE20", 20, DateTime(2022, 8, 17));
+    final today = DateTime(2022, 8, 1);
+    final isValid = coupon.isValid(today);
+    expect(isValid, isTrue);
+  });
+
+  test("Deve criar um cupom de desconto expirado", () {
+    final coupon = Coupon("VALE20", 20, DateTime(2022, 8, 11));
+    final today = DateTime(2022, 8, 13);
+    final isExpired = coupon.isExpired(today);
+    expect(isExpired, isTrue);
+  });
+
+  test("Deve criar um cupom de desconto e calcular o desconto", () {
+    final coupon = Coupon("VALE20", 20);
+    final discount = coupon.calculateDiscount(1000);
+    expect(discount, equals(200));
+  });
+}
