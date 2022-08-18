96b9b85e6087022d4ad448c824120c5242cc7fc7
derek-alves <derek.alves1998@hotmail.com>
2022-08-18 18:46:48 -0300

    step: create isExpired and calculate discounnt methods

--- project-1/lib/coupon.dart
+++ project-1/lib/coupon.dart
@@ -1,6 +1,22 @@
 class Coupon {
   final String code;
   final double percentage;
+  DateTime? expiredDate;
 
-  Coupon(this.code, this.percentage);
+  Coupon(this.code, this.percentage, [this.expiredDate]);
+  bool isValid([DateTime? today]) {
+    if (expiredDate == null) return true;
+    return expiredDate!.millisecondsSinceEpoch >=
+        (today?.millisecondsSinceEpoch ??
+            DateTime.now().millisecondsSinceEpoch);
+  }
+
+  bool isExpired([DateTime? today]) {
+    return !isValid(today);
+  }
+
+  double calculateDiscount(double total, [DateTime? date]) {
+    if (isExpired(date)) return 0;
+    return (total * percentage) / 100;
+  }
 }
