52b2fbcf97429df44fa33e589ea13310d43784cb
derek-alves <derek.alves1998@hotmail.com>
2022-08-18 19:29:46 -0300

    step: add new properties width,height,depth and weight add new methods to calculate freight

--- project-1/lib/item.dart
+++ project-1/lib/item.dart
@@ -3,11 +3,27 @@ class Item {
   final String category;
   final String description;
   final double price;
+  final double width;
+  final double height;
+  final double depth;
+  final double weight;
 
   Item({
     required this.id,
     required this.category,
     required this.description,
     required this.price,
+    this.width = 0,
+    this.height = 0,
+    this.depth = 0,
+    this.weight = 0,
   });
+
+  double getVolume() => (width / 100) * (height / 100) * (depth / 100);
+  double getDensity() => weight / getVolume();
+  double calculateFreight() {
+    final freight = (1000 * getVolume() * (getDensity() / 100));
+    if (freight < 10) return 10;
+    return freight;
+  }
 }
