e5a0857432ad8ec9378bc6c110c3ec9c27956523
derek-alves <derek.alves1998@hotmail.com>
2022-08-24 19:30:51 -0300

    step: implements in memory item repository

---
+++ project-1/lib/infra/repository/memory/item_repository_memory.dart
@@ -0,0 +1,18 @@
+import 'package:project/domain/entity/item.dart';
+import 'package:project/domain/repository/item_repository.dart';
+
+class ItemRepositoryMemory implements ItemRepository {
+  late final List items;
+  ItemRepositoryMemory() {
+    items = [
+      Item(id: 1, category: 'Musica', description: "CD", price: 30),
+      Item(id: 2, category: 'Video', description: "DVD", price: 50),
+      Item(id: 3, category: 'Video', description: "VHS", price: 10),
+    ];
+  }
+  @override
+  Future<Item?> findById(int id) {
+    // TODO: implement findById
+    throw UnimplementedError();
+  }
+}
