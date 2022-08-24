d8a14a0ab976fa1379ba9d3382c37b315c090ee6
derek-alves <derek.alves1998@hotmail.com>
2022-08-24 19:29:48 -0300

    step: add fromMap to the input and output

--- project-1/lib/application/usecase/place_order_input.dart
+++ project-1/lib/application/usecase/place_order_input.dart
@@ -2,12 +2,23 @@ class PlaceOrderInput {
   final String cpf;
   final List<OrderItemInput> orderItems;
   final DateTime date;
+  final String? coupon;
 
   PlaceOrderInput({
     required this.cpf,
     required this.orderItems,
     required this.date,
+    this.coupon,
   });
+
+  factory PlaceOrderInput.fromMap(Map<String, dynamic> map) => PlaceOrderInput(
+        cpf: map['cpf'],
+        orderItems: (map['orderItems'] as List)
+            .map((item) => OrderItemInput.fromMap(item))
+            .toList(),
+        date: map['date'],
+        coupon: map['coupon'],
+      );
 }
 
 class OrderItemInput {
@@ -15,4 +26,9 @@ class OrderItemInput {
   final int quantity;
 
   OrderItemInput(this.idItem, this.quantity);
+
+  factory OrderItemInput.fromMap(Map<String, dynamic> map) => OrderItemInput(
+        map["idItem"],
+        map["quantity"],
+      );
 }
