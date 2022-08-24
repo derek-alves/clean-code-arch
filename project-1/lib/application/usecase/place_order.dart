752ecde316d2f99dd8e59098f9b0cc1b0e50dc18
derek-alves <derek.alves1998@hotmail.com>
2022-08-24 19:30:10 -0300

    step: add order and coupon repository

--- project-1/lib/application/usecase/place_order.dart
+++ project-1/lib/application/usecase/place_order.dart
@@ -1,13 +1,21 @@
-import 'package:project/application/usecase/place_order_input.dart';
 import 'package:project/application/usecase/place_order_output.dart';
-import 'package:project/domain/entity/order.dart';
 
+import '../../domain/entity/order.dart';
+import '../../domain/repository/coupon_repository.dart';
 import '../../domain/repository/item_repository.dart';
+import '../../domain/repository/order_repository.dart';
+import 'place_order_input.dart';
 
 class PlaceOrder {
   final ItemRepository itemRepository;
+  final OrderRepository orderRepository;
+  final CouponRepository couponRepository;
 
-  PlaceOrder(this.itemRepository);
+  PlaceOrder({
+    required this.itemRepository,
+    required this.orderRepository,
+    required this.couponRepository,
+  });
 
   Future<PlaceOrderOutput> execute(PlaceOrderInput input) async {
     final order = Order(cpf: input.cpf, date: input.date);
@@ -16,7 +24,11 @@ class PlaceOrder {
       if (item == null) throw Exception("Item not found");
       order.addItem(item, quantity: orderItem.quantity);
     }
-
+    if (input.coupon != null) {
+      final coupon = await couponRepository.findByCode(input.coupon!);
+      if (coupon != null) order.addCoupon(coupon);
+    }
+    await orderRepository.save(order);
     return PlaceOrderOutput(order.total);
   }
 }
