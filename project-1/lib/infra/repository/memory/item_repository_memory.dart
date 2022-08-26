aa5e5be60f3583c51f1b46c594375a3952db429e
derek-alves <derek.alves1998@hotmail.com>
2022-08-26 00:46:38 -0300

    step: add others items

--- project-1/lib/infra/repository/memory/item_repository_memory.dart
+++ project-1/lib/infra/repository/memory/item_repository_memory.dart
@@ -10,6 +10,36 @@ class ItemRepositoryMemory implements ItemRepository {
       Item(id: 1, category: 'Musica', description: "CD", price: 30),
       Item(id: 2, category: 'Video', description: "DVD", price: 50),
       Item(id: 3, category: 'Video', description: "VHS", price: 10),
+      Item(
+        id: 4,
+        category: 'Instrumentos Musicais',
+        description: "guitarra",
+        price: 1000,
+        width: 100,
+        height: 30,
+        depth: 10,
+        weight: 3,
+      ),
+      Item(
+        id: 5,
+        category: 'Instrumentos Musicais',
+        description: "Amplificador",
+        price: 5000,
+        width: 100,
+        height: 50,
+        depth: 50,
+        weight: 20,
+      ),
+      Item(
+        id: 6,
+        category: 'Acessórios',
+        description: "Cabo",
+        price: 30,
+        width: 10,
+        height: 10,
+        depth: 10,
+        weight: 0.9,
+      ),
     ];
   }
   @override
