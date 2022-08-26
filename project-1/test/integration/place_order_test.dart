aa657154eae854765d818643736229a1d47a4fa8
derek-alves <derek.alves1998@hotmail.com>
2022-08-26 00:47:52 -0300

    step: finished the test

--- project-1/test/integration/place_order_test.dart
+++ project-1/test/integration/place_order_test.dart
@@ -1,10 +1,22 @@
 import 'package:project/application/usecase/place_order.dart';
 import 'package:project/application/usecase/place_order_input.dart';
+import 'package:project/infra/repository/memory/coupon_repository_memory.dart';
+import 'package:project/infra/repository/memory/item_repository_memory.dart';
+import 'package:project/infra/repository/memory/order_repository_memory.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Deve fazer um pedido', () async {
-    final placeOrder = PlaceOrder();
+    final itemRepository = ItemRepositoryMemory();
+    final orderRepository = OrderRepositoryMemory();
+    final couponRepository = CouponRepositoryMemory();
+
+    final placeOrder = PlaceOrder(
+      couponRepository: couponRepository,
+      itemRepository: itemRepository,
+      orderRepository: orderRepository,
+    );
+
     final inputMap = {
       'cpf': '457.046.588-90',
       'orderItems': [
@@ -18,6 +30,32 @@ void main() {
     final input = PlaceOrderInput.fromMap(inputMap);
 
     final output = await placeOrder.execute(input);
-    expect(output.total, equals(1000));
+    expect(output.total, equals(88));
+  });
+
+  test('Deve fazer um pedido com cálculo de frete', () async {
+    final itemRepository = ItemRepositoryMemory();
+    final orderRepository = OrderRepositoryMemory();
+    final couponRepository = CouponRepositoryMemory();
+
+    final placeOrder = PlaceOrder(
+      couponRepository: couponRepository,
+      itemRepository: itemRepository,
+      orderRepository: orderRepository,
+    );
+
+    final inputMap = {
+      'cpf': '457.046.588-90',
+      'orderItems': [
+        {"idItem": 4, "quantity": 1},
+        {"idItem": 5, "quantity": 1},
+        {"idItem": 6, "quantity": 3}
+      ],
+      'date': DateTime(2022, 08, 23),
+    };
+    final input = PlaceOrderInput.fromMap(inputMap);
+
+    final output = await placeOrder.execute(input);
+    expect(output.total, equals(6350));
   });
 }
