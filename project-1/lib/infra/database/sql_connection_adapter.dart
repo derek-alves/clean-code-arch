f57e059d4f74778fed3dd70cf8e69150f42e20bd
derek-alves <derek.alves1998@hotmail.com>
2022-09-03 23:48:39 -0300

    step: implements methods to the connection with database

--- project-1/lib/infra/database/sql_connection_adapter.dart
+++ project-1/lib/infra/database/sql_connection_adapter.dart
@@ -1,20 +1,35 @@
 import 'package:mysql1/mysql1.dart';
 import 'package:project/infra/database/connection.dart';
 
-class SqlConnectionAdapter implements Connection<MySqlConnection> {
-  @override
-  // TODO: implement connection
-  Future<MySqlConnection> get connection => throw UnimplementedError();
+import '../../utils/custom_env.dart';
 
+class SqlConnectionAdapter implements Connection<MySqlConnection> {
+  MySqlConnection? _connection;
   @override
-  Future<MySqlConnection> createConnection() {
-    // TODO: implement createConnection
-    throw UnimplementedError();
+  Future<MySqlConnection> get connection async {
+    _connection ??= await createConnection();
+    if (_connection == null) {
+      throw Exception("[ERROR/DB] -> Failed Create Connection");
+    }
+    return _connection!;
   }
 
   @override
-  Future query(String sql, [List? params]) {
-    // TODO: implement query
-    throw UnimplementedError();
+  Future<MySqlConnection> createConnection() async =>
+      await MySqlConnection.connect(ConnectionSettings(
+        host: await CustomEnv.get<String>(key: 'db_host'),
+        port: await CustomEnv.get<int>(key: 'db_port'),
+        user: await CustomEnv.get<String>(key: 'db_user'),
+        password: await CustomEnv.get<String>(key: 'db_pass'),
+        db: await CustomEnv.get<String>(key: 'db_schema'),
+      ));
+
+  @override
+  query(String sql, [List? params]) async {
+    var connection = await this.connection;
+    return await connection.query(
+      sql,
+      params,
+    );
   }
 }
