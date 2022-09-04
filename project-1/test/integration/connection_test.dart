70f5e1589e5b33f3f69f2557b485ebccabbf1cdd
derek-alves <derek.alves1998@hotmail.com>
2022-09-03 23:48:55 -0300

    step: add sql adapter in test

--- project-1/test/integration/connection_test.dart
+++ project-1/test/integration/connection_test.dart
@@ -1,9 +1,10 @@
+import 'package:project/infra/database/sql_connection_adapter.dart';
 import 'package:test/test.dart';
 
 void main() {
   test("Deve criar uma conexão com o banco de dados", (() async {
-    final connection = Connection();
-    final items = await connection.query("select * ffrom ccca.item", []);
+    final connection = SqlConnectionAdapter();
+    final items = await connection.query("select * from market.item", []);
     expect(items, isNotEmpty);
   }));
 }
