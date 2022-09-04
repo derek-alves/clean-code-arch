04802f90031fa8e21f742c6ef9263e2521ef2633
derek-alves <derek.alves1998@hotmail.com>
2022-09-03 23:47:58 -0300

    refactor: add id in order_item

--- project-1/lib/domain/entity/order_item.dart
+++ project-1/lib/domain/entity/order_item.dart
@@ -1,9 +1,11 @@
 class OrderItem {
+  final int? id;
   final int idItem;
   final double price;
   final int quantity;
 
   OrderItem({
+    this.id,
     required this.idItem,
     required this.price,
     required this.quantity,
