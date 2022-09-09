668db275faa6f664b52ed8781da4ce8f68e12230
derek-alves <derek.alves1998@hotmail.com>
2022-09-09 00:44:40 -0300

    refactor: change datetime type

--- project-1/test/unit/coupon_test.dart
+++ project-1/test/unit/coupon_test.dart
@@ -3,14 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   test("Deve criar um cupom de desconto válido", () {
-    final coupon = Coupon("VALE20", 20, DateTime(2022, 8, 17));
+    final coupon =
+        Coupon("VALE20", 20, DateTime(2022, 8, 17).millisecondsSinceEpoch);
     final today = DateTime(2022, 8, 1);
     final isValid = coupon.isValid(today);
     expect(isValid, isTrue);
   });
 
   test("Deve criar um cupom de desconto expirado", () {
-    final coupon = Coupon("VALE20", 20, DateTime(2022, 8, 11));
+    final coupon =
+        Coupon("VALE20", 20, DateTime(2022, 8, 11).millisecondsSinceEpoch);
     final today = DateTime(2022, 8, 13);
     final isExpired = coupon.isExpired(today);
     expect(isExpired, isTrue);
