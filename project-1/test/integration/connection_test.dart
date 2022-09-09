5f46d35dc0311828724b8204bd2a2736f302199e
derek-alves <derek.alves1998@hotmail.com>
2022-09-08 23:55:32 -0300

    step: finish tests

--- project-1/test/integration/connection_test.dart
+++ project-1/test/integration/connection_test.dart
@@ -1,10 +1,12 @@
+import 'package:mysql1/mysql1.dart';
 import 'package:project/infra/database/sql_connection_adapter.dart';
 import 'package:test/test.dart';
 
 void main() {
   test("Deve criar uma conexão com o banco de dados", (() async {
     final connection = SqlConnectionAdapter();
-    final items = await connection.query("select * from market.item", []);
+    final Results items =
+        await connection.query("select * from market.item", []);
     expect(items, isNotEmpty);
   }));
 }
