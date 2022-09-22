adea187bbbc54200c961a0876572b1079b6e9d68
derek-alves <derek.alves1998@hotmail.com>
2022-09-22 19:47:38 -0300

    step: implements themes in view

--- design-patterns/lib/creational/abstract_factory/view.dart
+++ design-patterns/lib/creational/abstract_factory/view.dart
@@ -1,10 +1,18 @@
+import 'button.dart';
+import 'dark_button.dart';
 import 'label.dart';
+import 'light_button.dart';
 
 class View {
-  Label label;
-  Button button;
-  View() {
-    label = Label();
-    button = Button();
+  late final Label label;
+  late final Button button;
+  View(String theme) {
+    if (theme.toLowerCase() == 'dark') {
+      label = Label();
+      button = DarkButton();
+    } else {
+      label = Label();
+      button = LightButton();
+    }
   }
 }
