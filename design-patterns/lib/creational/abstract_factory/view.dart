279b96ebf77f59a13f2ad9d5688c9b3da99758e0
derek-alves <derek.alves1998@hotmail.com>
2022-09-23 19:30:08 -0300

    step: add widget factory

--- design-patterns/lib/creational/abstract_factory/view.dart
+++ design-patterns/lib/creational/abstract_factory/view.dart
@@ -1,18 +1,13 @@
+import 'package:design_patterns/creational/abstract_factory/widget_factory.dart';
+
 import 'button.dart';
-import 'dark_button.dart';
 import 'label.dart';
-import 'light_button.dart';
 
 class View {
   late final Label label;
   late final Button button;
-  View(String theme) {
-    if (theme.toLowerCase() == 'dark') {
-      label = Label();
-      button = DarkButton();
-    } else {
-      label = Label();
-      button = LightButton();
-    }
+  View(WidgetFactory widgetFactory) {
+    label = widgetFactory.createLabel();
+    button = widgetFactory.createButton();
   }
 }
