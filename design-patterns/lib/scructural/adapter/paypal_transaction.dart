a273e27dd3cd35a0fc7408d6d919cc101c63d6f5
derek-alves <derek.alves1998@hotmail.com>
2022-09-29 00:32:56 -0300

    step: create stripe and paypal transactions

---
+++ design-patterns/lib/scructural/adapter/paypal_transaction.dart
@@ -0,0 +1,7 @@
+class PaypalTransaction {
+  final int id;
+  final double amount;
+  final String status;
+
+  PaypalTransaction(this.id, this.amount, this.status);
+}
---
+++ design-patterns/lib/scructural/adapter/stripe_transaction.dart
@@ -0,0 +1,7 @@
+class StripeTransaction {
+  final String code;
+  final double amount;
+  final int situation;
+
+  StripeTransaction(this.code, this.amount, this.situation);
+}
