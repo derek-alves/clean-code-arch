7a6987e4d1e2cde06e986c83747a2e8eaca11fb5
derek-alves <derek.alves1998@hotmail.com>
2022-10-30 13:23:01 -0300

    step: create checkout method

--- design-patterns/lib/behavioral/strategy/parking_lot.dart
+++ design-patterns/lib/behavioral/strategy/parking_lot.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
+
 class ParkingLot {
   final int capacity;
-  final List<Map<String, dynamic>> _parkedCars = [];
+  final List<ParkedCar> _parkedCars = [];
   ParkingLot(this.capacity);
 
   getEmptySpaces() {
@@ -8,6 +10,24 @@ class ParkingLot {
   }
 
   checkin(String plate, DateTime date) {
-    _parkedCars.add({"plate": plate, "checkinData": date.toString()});
+    _parkedCars.add(ParkedCar(plate, date));
   }
+
+  double checkout(String plate, DateTime date) {
+    final parkedCar = _parkedCars.firstWhereOrNull((car) => car.plate == plate);
+    if (parkedCar == null) {
+      throw Exception("Car not found");
+    }
+    _parkedCars.removeWhere((parkendCar) => parkendCar.plate == plate);
+    final diff = date.difference(parkedCar.date).inHours;
+
+    return diff * 10;
+  }
+}
+
+class ParkedCar {
+  final String plate;
+  final DateTime date;
+
+  ParkedCar(this.plate, this.date);
 }
