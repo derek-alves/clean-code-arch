00e6b5ed361ff44e14b1211cf18b3e380f9f4f71
derek-alves <derek.alves1998@hotmail.com>
2022-10-31 19:30:13 -0300

    refactor: pass price calculator to the constructor

--- design-patterns/lib/behavioral/strategy/parking_lot.dart
+++ design-patterns/lib/behavioral/strategy/parking_lot.dart
@@ -1,9 +1,16 @@
 import 'package:collection/collection.dart';
+import 'package:design_patterns/behavioral/strategy/price_calculator.dart';
 
 class ParkingLot {
   final int capacity;
   final List<ParkedCar> _parkedCars = [];
-  ParkingLot(this.capacity);
+  final String location;
+  final PriceCalculator priceCalculator;
+  ParkingLot(
+    this.capacity, {
+    this.location = "shopping",
+    required this.priceCalculator,
+  });
 
   getEmptySpaces() {
     return capacity - _parkedCars.length;
@@ -18,10 +25,23 @@ class ParkingLot {
     if (parkedCar == null) {
       throw Exception("Car not found");
     }
+
     _parkedCars.removeWhere((parkendCar) => parkendCar.plate == plate);
     final diff = date.difference(parkedCar.date).inHours;
 
-    return diff * 10;
+    // if (location == "shopping") {
+    //   return diff * 10;
+    // }
+
+    // if (location == "airport") {
+    //   double price = 20.0;
+    //   int remainingHours = diff - 3;
+    //   if (remainingHours <= 0) return price;
+    //   price += remainingHours * 10;
+    //   return price;
+    // }
+
+    return priceCalculator.calculate(hours: diff);
   }
 }
 
