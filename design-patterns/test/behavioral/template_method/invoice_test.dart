6f5f3fce1179bf3df15d3231a3630c7adc90954e
derek-alves <derek.alves1998@hotmail.com>
2022-11-07 23:30:39 -0300

    refactor: change generic item to specific product

--- design-patterns/test/behavioral/template_method/invoice_test.dart
+++ design-patterns/test/behavioral/template_method/invoice_test.dart
@@ -1,13 +1,15 @@
+import 'package:design_patterns/behavioral/template_method/beer.dart';
 import 'package:design_patterns/behavioral/template_method/invoice.dart';
-import 'package:design_patterns/behavioral/template_method/item.dart';
+import 'package:design_patterns/behavioral/template_method/water.dart';
+import 'package:design_patterns/behavioral/template_method/whisky.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Deve criar uma nota fiscal', () async {
     final invoice = Invoice();
-    invoice.addItem(Item("Beer", "Heineken", 10));
-    invoice.addItem(Item("Whisky", "Jack Daniels", 20));
-    invoice.addItem(Item("Water", "Santa Catarina", 0));
+    invoice.addItem(Beer("Heineken", 10));
+    invoice.addItem(Whisky("Jack Daniels", 20));
+    invoice.addItem(Water("Santa Catarina", 0));
 
     final taxes = invoice.getTaxes();
     expect(taxes, equals(21));
