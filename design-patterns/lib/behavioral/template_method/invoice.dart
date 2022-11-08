b8c9a99d08c280c5ef32f5f56740afd19f16221c
derek-alves <derek.alves1998@hotmail.com>
2022-11-08 17:23:32 -0300

    step: add validation when item implements texItem

--- design-patterns/lib/behavioral/template_method/invoice.dart
+++ design-patterns/lib/behavioral/template_method/invoice.dart
@@ -1,4 +1,5 @@
 import 'package:design_patterns/behavioral/template_method/item.dart';
+import 'package:design_patterns/behavioral/template_method/tax_item.dart';
 
 class Invoice {
   late final List<Item> _item;
@@ -11,7 +12,11 @@ class Invoice {
 
   num getTaxes() {
     num taxes = 0;
-    for (Item item in _item) {}
+    for (Item item in _item) {
+      if (item is TaxItem) {
+        taxes += item.getTaxes();
+      }
+    }
 
     return taxes;
   }
