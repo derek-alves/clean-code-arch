7d9ec431b8efc5fcfdb77330a85821b8d6ea0f7b
derek-alves <derek.alves1998@hotmail.com>
2022-11-11 01:22:32 -0300

    step: implement response to route

--- project-1/lib/order_api.dart
+++ project-1/lib/order_api.dart
@@ -1,12 +1,24 @@
 import 'package:project/api_handler.dart';
+import 'package:project/application/usecase/place_order/place_order.dart';
+import 'package:project/infra/factory/make_database_repository_factory.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class OrderApi extends ApiHandler {
+  final respositoryFactory = makeDatabaseRepositoryFactory();
   @override
   Handler getHandler() {
     var router = Router();
-    router.post("/orders", (Request request) {});
+    router.post("/orders", (Request request) async {
+      var placeOrder = PlaceOrderUsecase(respositoryFactory);
+      var input = PlaceOrderInput.fromRequest(await request.readAsString());
+      try {
+        await placeOrder.execute(input);
+        return Response(200);
+      } on Exception catch (e) {
+        return Response.notFound(e);
+      }
+    });
 
     return createHandler(handler: router);
   }
