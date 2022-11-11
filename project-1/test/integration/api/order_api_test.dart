a7bc5d4a22754af765f690e613a6fff46ac0ace3
derek-alves <derek.alves1998@hotmail.com>
2022-11-11 01:23:28 -0300

    step: create integration test for order api

---
+++ project-1/test/integration/api/order_api_test.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+import 'package:project/utils/custom_env.dart';
+import 'package:test/test.dart';
+import 'package:dio/dio.dart';
+
+void main() {
+  late final Dio dio;
+  late final String url;
+  setUpAll(() async {
+    dio = Dio();
+    String serverAdress = await CustomEnv.get(key: "server_address");
+    String serverPort = await CustomEnv.get(key: "server_port");
+    url = 'https://${serverAdress}:${serverPort}';
+  });
+  test("Should create order", () async {
+    var response = await dio.post(url, data: {
+      'cpf': '457.046.588-90',
+      'orderItems': [
+        {"idItem": 1, "quantity": 1},
+        {"idItem": 2, "quantity": 1},
+        {"idItem": 3, "quantity": 3}
+      ],
+      'date': DateTime(2022, 11, 11).microsecondsSinceEpoch,
+      'coupon': "VALE20"
+    });
+    expect(response.statusCode, equals(200));
+  });
+}
