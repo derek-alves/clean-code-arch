6bb6271580f053e3ab2901fef815057210f960d5
derek-alves <derek.alves1998@hotmail.com>
2022-11-11 19:50:53 -0300

    step: implements simulate freight route

--- project-1/lib/order_api.dart
+++ project-1/lib/order_api.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:project/api_handler.dart';
 import 'package:project/application/usecase/place_order/place_order.dart';
 import 'package:project/infra/factory/make_database_repository_factory.dart';
+import 'package:project/infra/factory/make_simulate_freight_factory.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -9,7 +12,7 @@ class OrderApi extends ApiHandler {
   @override
   Handler getHandler() {
     var router = Router();
-    router.post("/orders", (Request request) async {
+    router.post("/order", (Request request) async {
       var placeOrder = PlaceOrderUsecase(respositoryFactory);
       var input = PlaceOrderInput.fromRequest(await request.readAsString());
       try {
@@ -18,6 +21,12 @@ class OrderApi extends ApiHandler {
       } on Exception catch (e) {
         return Response.notFound(e);
       }
+    });
+
+    router.get("/order/simulateFreight", (Request request) async {
+      final simulateFreight = makeSimulateFreightFactory();
+      var input = jsonDecode(await request.readAsString());
+      return Response.ok("TEste");
     });
 
     return createHandler(handler: router);
