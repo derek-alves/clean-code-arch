62ba3160fefc8a6e8bbc9786f2921e15a1e59e9a
derek-alves <derek.alves1998@hotmail.com>
2022-11-11 19:52:35 -0300

    refactor: add http instead of https

--- project-1/test/integration/api/order_api_test.dart
+++ project-1/test/integration/api/order_api_test.dart
@@ -8,21 +8,24 @@ void main() {
   late final Dio dio;
   late final String url;
   setUpAll(() async {
-    dio = Dio();
+    dio = Dio(BaseOptions(
+      connectTimeout: 5000,
+      receiveTimeout: 5000,
+    ));
     String serverAdress = await CustomEnv.get(key: "server_address");
     String serverPort = await CustomEnv.get(key: "server_port");
-    url = 'https://${serverAdress}:${serverPort}';
+    url = 'http://${serverAdress}:${serverPort}/';
   });
   test("Should create order", () async {
-    var response = await dio.post(url, data: {
-      'cpf': '457.046.588-90',
-      'orderItems': [
+    var response = await dio.post(url + "orders", data: {
+      "cpf": "457.046.588-90",
+      "orderItems": [
         {"idItem": 1, "quantity": 1},
         {"idItem": 2, "quantity": 1},
         {"idItem": 3, "quantity": 3}
       ],
-      'date': DateTime(2022, 11, 11).microsecondsSinceEpoch,
-      'coupon': "VALE20"
+      "date": DateTime(2022, 11, 11).microsecondsSinceEpoch,
+      "coupon": "VALE20"
     });
     expect(response.statusCode, equals(200));
   });
