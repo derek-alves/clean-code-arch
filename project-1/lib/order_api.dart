caf290da02ac570d2dbb9a431038e6d1daad4929
derek-alves <derek.alves1998@hotmail.com>
2022-11-14 20:18:52 -0300

    step: implements simulate freight

--- project-1/lib/order_api.dart
+++ project-1/lib/order_api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:project/api_handler.dart';
 import 'package:project/application/usecase/place_order/place_order.dart';
+import 'package:project/application/usecase/simulate_freight/simulate_freight_input.dart';
 import 'package:project/infra/factory/make_database_repository_factory.dart';
 import 'package:project/infra/factory/make_simulate_freight_factory.dart';
 import 'package:shelf/shelf.dart';
@@ -23,10 +24,21 @@ class OrderApi extends ApiHandler {
       }
     });
 
-    router.get("/order/simulateFreight", (Request request) async {
-      final simulateFreight = makeSimulateFreightFactory();
-      var input = jsonDecode(await request.readAsString());
-      return Response.ok("TEste");
+    router.post("/order/simulate-freight", (Request request) async {
+      try {
+        final simulateFreight = makeSimulateFreightFactory();
+        var input = jsonDecode(await request.readAsString());
+
+        bool isTrue = input.toString() is List<Map<String, dynamic>>;
+        if (input is! List<Map<String, dynamic>>) {
+          return Response.badRequest();
+        }
+
+        var response = await simulateFreight(SimulateFreightInput(input));
+        return Response.ok(response.toJson());
+      } catch (e) {
+        return Response.notFound(e);
+      }
     });
 
     return createHandler(handler: router);
