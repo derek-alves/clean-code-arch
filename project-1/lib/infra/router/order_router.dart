ba95df0c8f63cdde7157a3ab1eda3f9ae1740487
derek-alves <derek.alves1998@hotmail.com>
2022-11-18 01:28:23 -0300

    step: create order router

---
+++ project-1/lib/infra/router/order_router.dart
@@ -0,0 +1,23 @@
+import 'package:project/infra/server/api_handler.dart';
+import 'package:project/infra/controller/order_controller.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class OrderRouter extends ApiHandler {
+  final orderController = OrderController();
+  @override
+  Handler getHandler() {
+    var router = Router();
+    router.post(
+      "/order",
+      (Request request) async => await orderController.placeOrder(request),
+    );
+
+    router.post(
+      "/order/simulate-freight",
+      (Request request) async => await orderController.simulateFreight(request),
+    );
+
+    return createHandler(handler: router);
+  }
+}
