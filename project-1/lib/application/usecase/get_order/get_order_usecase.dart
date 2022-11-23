1cd5dab949b2d447ce65f14d04a547481de4f531
derek-alves <derek.alves1998@hotmail.com>
2022-11-23 14:36:46 -0300

    step: create get order usecase

---
+++ project-1/lib/application/usecase/get_order/get_order_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:project/domain/factory/repository_factory.dart';
+import 'package:project/domain/repository/order_repository.dart';
+
+import 'get_order_output.dart';
+
+class GetOrderUsecase {
+  final RepositoryFactory _repositoryFactory;
+  late final OrderRepository _orderRepository;
+  GetOrderUsecase(this._repositoryFactory) {
+    _orderRepository = _repositoryFactory.createOrderRepository();
+  }
+
+  Future<GetOrderOutput> call(String code) async {
+    final order = await _orderRepository.get(code);
+    return GetOrderOutput(total: order.total);
+  }
+}
